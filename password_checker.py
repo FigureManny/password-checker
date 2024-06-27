6e9660e003cafc734394bef1a854682458239619
Manuel Folgar <manuel.folgarberrios@csedge.org>
2024-06-27 14:15:49 -0700

    initialize

---
+++ password_checker.py
@@ -0,0 +1,26 @@
+test = "manuel"
+first_char = test[0]
+print(first_char.isupper())
+
+# islower()
+
+print(test.find("a"))
+#return the index if it finds it 
+#returns -1 if it cant find it 
+
+#step 1
+#prompt the user to enter their password
+#check the following:
+# - if the password contains the letter z
+# - if the password ends with an !
+# - if the password begins with an uppercase letter
+#if yes to all three, print accept
+#if any of these are not true, print reject
+
+password = input("please make a password ")
+password = password.strip()
+if "z" in password and password[-1] == "!" and password[0].isupper():
+    print("accept")
+else:
+    print("please try again ")
+
